54b23b5c46413c3b9f89b6b22e9fde6ab2a7774c
Guilherme <borimdev@gmail.com>
2023-09-06 15:27:48 -0300

    inicializacao do projeto e configuracao dos arquivos

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EStiloGlobal, { Container } from './styles';
+
+function App() {
+  return (
+    <div>
+      <EStiloGlobal />
+      <Container>app</Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles.jsx
@@ -0,0 +1,30 @@
+import { createGlobalStyle, styled } from 'styled-components';
+
+const EStiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Inter', sans-serif;
+  }
+  ul,li,a {
+    list-style: none;
+    text-decoration: none;
+  }
+  h1,h2,h3,h4,h5,h6 {
+    margin: 0;
+    padding: 0;
+  }
+  img {
+    display: block;
+    max-width: 100%;
+  }
+`;
+
+export default EStiloGlobal;
+
+export const Container = styled.div`
+  max-width: 1170px;
+  width: 100%;
+  margin: 0 auto;
+`;
